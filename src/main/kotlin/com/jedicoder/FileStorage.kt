c53d0604348f31658f74761dc485c71848fde945
Alex Slyvets <slivets.aleksay@gmail.com>
2019-12-22 19:57:19 +0100

    implement rest of CRUD

--- src/main/kotlin/com/jedicoder/FileStorage.kt
+++ src/main/kotlin/com/jedicoder/FileStorage.kt
@@ -8,7 +8,7 @@ class FileStorage : Storage {
 
     override fun create(storeName: String, entry: String): Boolean {
         val file = File(storeName)
-        val entries =readEntries(file)
+        val entries = readEntries(file)
         return if (entries.contains(entry)) {
             logger.warn("Can create an entry $entry in the file $storeName because it is already present")
             false
@@ -20,6 +20,39 @@ class FileStorage : Storage {
         }
     }
 
+    override fun update(storeName: String, oldEntry: String, newEntry: String): Boolean {
+        val file = File(storeName)
+        val entries = readEntries(file)
+        val indexOf = entries.indexOf(oldEntry)
+        return if (indexOf == -1) {
+            logger.warn("Entry $oldEntry doesn't exist in the storage $storeName")
+            false
+        } else {
+            val strings = entries.toTypedArray()
+            strings[indexOf] = newEntry
+            val appendedList = strings.joinToString("\n")
+            file.writeText(appendedList)
+            logger.info("Entry $newEntry was successfully updated in the storage $storeName")
+            true
+        }
+    }
+
+    override fun delete(storeName: String, entry: String): Boolean {
+        val file = File(storeName)
+        val entries = readEntries(file)
+        val list = entries.toMutableList()
+        val result = list.remove(entry)
+        return if (result) {
+            val appendedList = list.joinToString("\n")
+            file.writeText(appendedList)
+            logger.info("Record $entry successfully deleted from the storage $storeName")
+            true
+        } else {
+            logger.warn("Entry $entry doesn't exist in the storage $storeName")
+            false
+        }
+    }
+
     private fun readEntries(file: File): List<String> {
         return try {
             file.readLines()
@@ -28,22 +61,11 @@ class FileStorage : Storage {
         }
     }
 
-    override fun update(storeName: String, entry: String): Boolean {
-        logger.info("Entry $entry was successfully updated in the storage $storeName")
-
-        return true
-    }
-
     override fun read(storeName: String): List<String> {
-        val emptyList = emptyList<String>()
+        val file = File(storeName)
+        val emptyList = readEntries(file)
         logger.info("Successfully read ${emptyList.size} entries from the storage $storeName")
 
         return emptyList
-    }
-
-    override fun delete(storeName: String, entry: String): Boolean {
-        logger.info("Record $entry successfully deleted from the storage $storeName")
-
-        return true
     }
 }
--- src/test/kotlin/com/jedicoder/FileStorageTest.kt
+++ src/test/kotlin/com/jedicoder/FileStorageTest.kt
@@ -2,13 +2,12 @@ package com.jedicoder
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
-import org.junit.Ignore
 import org.junit.Test
 import java.io.File
 
 class FileStorageTest {
-    private  val entry = "dummyEntry"
-    private  val storageName = "dummyName"
+    private val entry = "dummyEntry"
+    private val storageName = "dummyName"
 
     private val storage: Storage = FileStorage()
 
@@ -28,34 +27,33 @@ class FileStorageTest {
         assertThat(storage.create(storageName, entry)).isTrue()
     }
 
-    @Ignore
     @Test
     fun `when an entry was not updated then return false`() {
-        assertThat(storage.update(storageName, entry, "new")).isFalse()
+        assertThat(storage.update(storageName, entry, entry)).isFalse()
     }
 
     @Test
     fun `when an entry was successfully updated in the file then return true`() {
+        storage.create(storageName, entry)
         assertThat(storage.update(storageName, entry, "new")).isTrue()
     }
 
-    @Ignore
     @Test
     fun `when read a storage then return not empty list of entries`() {
+        storage.create(storageName, entry)
         val entries = storage.read(storageName)
 
         assertThat(entries).isNotEmpty
     }
 
-    @Ignore
     @Test
     fun `when an entry wasn't deleted then return false`() {
         assertThat(storage.delete(storageName, entry)).isFalse()
     }
 
-    @Ignore
     @Test
     fun `when an entry was successfully deleted then return true`() {
+        storage.create(storageName, entry)
         assertThat(storage.delete(storageName, entry)).isTrue()
     }
 }
