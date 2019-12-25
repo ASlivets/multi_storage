e0f6ca1ca3efd2d70516131514c20f557f00704c
Alex Slyvets <slivets.aleksay@gmail.com>
2019-12-25 21:34:48 +0100

    add test for static resource mapping

--- src/main/kotlin/com/jedicoder/CrudController.kt
+++ src/main/kotlin/com/jedicoder/CrudController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.ModelAndView
 class CrudController(private val storage: Storage) {
 
     @RequestMapping("/v1/create")
-    fun resources(
+    fun create(
         @RequestParam(
             "storeName",
             required = true
@@ -23,5 +23,53 @@ class CrudController(private val storage: Storage) {
         val isCreated = storage.create(storeName, entry)
 
         return ModelAndView("result", mapOf("status" to isCreated))
+    }
+
+    @RequestMapping("/v1/update")
+    fun resources(
+        @RequestParam(
+            "storeName",
+            required = true
+        ) storeName: String,
+        @RequestParam(
+            "oldValue",
+            required = true
+        ) oldValue: String,
+        @RequestParam(
+            "newValue",
+            required = true
+        ) newValue: String
+    ): ModelAndView {
+        val isUpdated = storage.update(storeName, oldValue, newValue)
+
+        return ModelAndView("result", mapOf("status" to isUpdated))
+    }
+
+    @RequestMapping("/v1/read")
+    fun read(
+        @RequestParam(
+            "storeName",
+            required = true
+        ) storeName: String
+    ): ModelAndView {
+        val entries = storage.read(storeName)
+
+        return ModelAndView("result", mapOf("entries" to entries))
+    }
+
+    @RequestMapping("/v1/delete")
+    fun delete(
+        @RequestParam(
+            "storeName",
+            required = true
+        ) storeName: String,
+        @RequestParam(
+            "entry",
+            required = true
+        ) entry: String
+    ): ModelAndView {
+        val isDeleted = storage.delete(storeName, entry)
+
+        return ModelAndView("result", mapOf("status" to isDeleted))
     }
 }
